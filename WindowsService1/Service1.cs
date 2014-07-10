550fb24b32b0e9a2b86ce367469421a45bc28b7d
John Sater <john.sater@valpo.edu>
2014-07-09 20:08:30 -0500

    Added a service installer
    I'm watching a tutorial, so I'm doing what it says for now

--- WindowsService1/Service1.cs
+++ WindowsService1/Service1.cs
@@ -8,21 +8,33 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsService1
-{
-    public partial class Service1 : ServiceBase
-    {
-        public Service1()
-        {
+namespace WindowsService1{
+
+    public partial class Service1 : ServiceBase{
+
+        public Service1(){
             InitializeComponent();
         }
 
-        protected override void OnStart(string[] args)
-        {
+        protected override void OnStart(string[] args){
+            Stopwatch stopWatch = new Stopwatch();  //we'll start by starting a new stopwatch
+            stopWatch.Start();                      //tracking the user's time logged in shall commence     
+
+            sampleFileCreation(); //I'm following a tutorial
         }
 
-        protected override void OnStop()
-        {
+        private void sampleFileCreation(){
+            string filePath = @"C:\Users\Dev\Documents\visual studio 2013\Projects\WindowsService1\WindowsService1\sample.txt";
+            string dateTime = DateTime.Now.ToString();
+
+            System.IO.StreamWriter fileWriter = new System.IO.StreamWriter(filePath, true);
+            fileWriter.WriteLine("\n" + dateTime);
+            fileWriter.Close();
+
+        }
+
+        protected override void OnStop(){
+
         }
     }
 }
