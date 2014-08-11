1c16db4ccd49a36b73d464d79d2f1e1405edea64
John Sater <john.sater@valpo.edu>
2014-08-10 22:17:17 -0500

    Add and XML settings class

---
+++ WindowsService1/MySettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WindowsService1
+{
+    /*
+     * This is 'generic' code for writing data to a settings file
+     */
+
+    public class MySettings
+    {
+        public string DatabaseConnection { get; set; }
+        public string LastAction { get; set; }
+        private static XmlSerializer xs;
+        static MySettings()
+        {
+            xs = new XmlSerializer(typeof(MySettings));
+        }
+        public void SaveToFile(string NameFile)
+        {
+            using (StreamWriter sr = new StreamWriter(NameFile))
+            {
+                xs.Serialize(sr, this);
+            }
+        }
+        public static MySettings ReadFromFile(string NameFile)
+        {
+            using(StreamReader sr = new StreamReader(NameFile))
+            {
+                return xs.Deserialize(sr) as MySettings;
+            }
+        }
+    }
+}
