4609e2939212c78929ad1c1888522837539a1560
John Sater <john.sater@valpo.edu>
2014-09-27 23:13:53 -0500

    new class! will be saving data to dataset!

---
+++ WindowsService1/HistoryStorage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    class HistoryStorage
+    {
+        private static DataSet _History;
+
+        private static String _FilePath = AppDomain.CurrentDomain.BaseDirectory + "\\sample.xml";
+        
+
+        static HistoryStorage()
+        {
+            _History = new DataSet();
+            
+            if (File.Exists(@_FilePath))
+            {
+                _History.ReadXml(_FilePath);   //this needs testing
+            }
+        }
+
+
+        public void WriteDataSet()
+        {
+            _History.WriteXml(_FilePath);
+        }
+
+
+        public void AddDataTable(DataTable dt)
+        {
+            _History.Tables.Add(dt);
+        }
+
+
+    }
+}
